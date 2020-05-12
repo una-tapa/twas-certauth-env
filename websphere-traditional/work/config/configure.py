9ee86256dfe62376120c476d112aec41ec9f6783
Hiroko Takamiya <htakamiy@us.ibm.com>
2020-05-11 21:28:59 -0400

    update

--- websphere-traditional/work/config/configure.py
+++ websphere-traditional/work/config/configure.py
@@ -2,6 +2,10 @@
 # AdminConfig.modify('(cells/DefaultCell01/nodes/DefaultNode01|serverindex.xml#ServerIndex_1)',  "[[hostName websphere]]")
 # AdminConfig.save()
 
+#Install Application
+AdminApp.install('/work/app/CertLoginSample.war', '[ -nopreCompileJSPs -distributeApp -nouseMetaDataFromBinary -appname CertLoginSample_war -createMBeansForResources -noreloadEnabled -nodeployws -validateinstall warn -noprocessEmbeddedConfig -filepermission .*\.dll=755#.*\.so=755#.*\.a=755#.*\.sl=755 -noallowDispatchRemoteInclude -noallowServiceRemoteInclude -asyncRequestDispatchType DISABLED -nouseAutoLink -noenableClientModule -clientMode isolated -novalidateSchema -contextroot /CertLoginSample -MapModulesToServers [[ "Hello Servlet" CertLoginSample.war,WEB-INF/web.xml WebSphere:cell=DefaultCell01,node=DefaultNode01,server=server1 ]] -MapWebModToVH [[ "Hello Servlet" CertLoginSample.war,WEB-INF/web.xml default_host ]]]' ) 
+AdminConfig.save()
+
 #Enable App security
 AdminTask.applyWizardSettings('[-secureApps true -secureLocalResources false -adminPassword password -userRegistryType WIMUserRegistry -adminName wsadmin ]')  
 AdminConfig.save()
